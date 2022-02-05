9dca37defb75c1b975602abef47c52f4747ce606
danpung2 <danpung_2@naver.com>
2022-02-05 20:31:19 +0900

    chapter 10

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -4,9 +4,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from mysite.views import HomeView
+from mysite.views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
     path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
